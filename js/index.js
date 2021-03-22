0642e1b395dfd4bdb84c507f2fb0d4648d47199a
William Fiallos <wfiallos10@gmail.com>
2021-03-22 16:08:36 -0400

    iteration 3 complete

--- js/index.js
+++ js/index.js
@@ -22,3 +22,36 @@ if (hacker1Length === hacker2Length) {
 }
 
 // Iteration 3: Loops
+// option 1:
+console.log(hacker1.toUpperCase('').split('').join(' '));
+// option 2:
+const hacker1Capitals = hacker1.toUpperCase();
+let hacker1Chars = "";
+for(let i = 0; i<hacker1Length; i++) {
+  hacker1Chars += hacker1Capitals[i] + ' ';
+}
+console.log(hacker1Chars.trim());
+
+// reversed
+let hacker2Reversed = "";
+for (let i = hacker2Length - 1; i >= 0; i--) {
+  hacker2Reversed += hacker2[i];
+} 
+console.log(hacker2Reversed);
+
+// lexicographic order
+let comparisonResult = '';
+for (let i = 0; i <shortestString.length; i++) {
+  if (hacker1[i] > hacker2[i]) {
+    console.log("The Driver's name goes first");
+    break;
+
+  } else if (hacker1[i] < hacker2[i]){
+    console.log("Yo, the navigator goes first definitely");
+    break;
+
+  } else {
+    console.log("What?! You both got the same name?");
+  }
+}
+console.log(comparisonResult);
